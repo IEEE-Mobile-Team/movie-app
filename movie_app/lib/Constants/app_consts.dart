63dbb71cc3fa16e9df68c8f469f11ee292aeaa9c
abdullah yehia <abdullah.yehia19@gmail.com>
2022-11-16 01:55:58 +0200

    repo created

---
+++ movie_app/lib/Constants/app_consts.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+Color background_color=const Color(0xFF000000);
+Color active_color=const Color(0xFFFFFFFF);
+Color idle_color=const Color(0xFF3A3939);
+Color txt_color=const Color(0xFFFFFFFF);
+
+double side_padding = 15;
---
+++ movie_app/lib/Pages/trending.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Trending extends StatelessWidget {
+  const Trending({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
